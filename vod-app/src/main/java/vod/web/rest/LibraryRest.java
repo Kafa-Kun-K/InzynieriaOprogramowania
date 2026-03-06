4666e9f06c2b027179ef989d8e7809d2488cb92e
KafaKunK <martinibod@gmail.com>
2026-03-06 13:58:46 +0100

    Zad6

--- vod-app/src/main/java/vod/web/rest/LibraryRest.java
+++ vod-app/src/main/java/vod/web/rest/LibraryRest.java
@@ -1,16 +1,22 @@
 package vod.web.rest;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.LocaleResolver;
 import vod.model.Book;
 import vod.model.Library;
 import vod.service.BookService;
 import vod.service.LibraryService;
 
 import java.util.List;
+import java.util.Locale;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,6 +26,8 @@ public class LibraryRest {
 
     private final LibraryService libraryService;
     private final BookService bookService;
+    private final MessageSource messageSource;
+    private final LocaleResolver localeResolver;
 
     @GetMapping("/libraries")
     List<Library> getLibraries(
@@ -28,6 +36,9 @@ public class LibraryRest {
             @CookieValue(value = "some-cookie", required = false) String someCookie
     ) {
         log.info("about to retrieve libraries list");
+        log.info("phrase param: {}", phrase);
+        log.info("custom header param: {}", customHeader);
+        log.info("some cookie value: {}", someCookie);
         List<Library> libraries = libraryService.getAllLibraries();
         log.info("{} libraries found", libraries.size());
         return libraries;
@@ -59,8 +70,17 @@ public class LibraryRest {
     }
 
     @PostMapping("/libraries")
-    ResponseEntity<Library> addLibrary(@RequestBody Library library) {
+    ResponseEntity<?> addLibrary(@Validated @RequestBody Library library, Errors errors, HttpServletRequest request) {
         log.info("about to add new library {}", library);
+
+        if(errors.hasErrors()) {
+            Locale locale = localeResolver.resolveLocale(request);
+            String errorMessage = errors.getAllErrors().stream()
+                    .map(oe->messageSource.getMessage(oe.getCode(), new Object[0], locale))
+                    .reduce("errors:\n", (accu, oe)->accu + oe + "\n");
+            return ResponseEntity.badRequest().body(errorMessage);
+        }
+
         library = libraryService.addLibrary(library);
         log.info("new library add {}", library);
         return ResponseEntity.status(HttpStatus.CREATED).body(library);
